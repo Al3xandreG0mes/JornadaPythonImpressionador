f65612a565d9bd1180ba5b8248fb19aca1956c9f
Alexandre Gomes <dev.alx.gm7@gmail.com>
2022-10-26 07:02:10 -0300

    Exercicio de estruturas sequenciais

---
+++ Estruturas_sequenciais.py
@@ -0,0 +1,11 @@
+forma 1 de se fazer essa soma utilizando fstring
+num1 = int(input('Digite o primeiro numero pra soma: '))
+num2 = int(input('Digite o segundo numero pra soma: '))
+soma = num1 + num2
+print(f'resultado da soma: {soma}\n')3
+
+#forma2 de faazer: 
+
+#num1 = int(input('Digite o primeiro numero da soma: '))
+#num2 = int(input('Digite o segundo numeri da soma: '))
+#print('resultado: ', num1+num2)
